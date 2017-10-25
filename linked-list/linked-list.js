9aaa514483ec3980a3c28dc8c93f15d48a4d3a5a
Charissa Johnson <charissa.y.johnson@gmail.com>
2017-10-25 10:57:01 -0500

    Added more functions to linked-list.js

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -38,6 +38,57 @@ LinkedList.prototype.addToTail = function(value){
     this.tail = newNode;
 }
 
+LinkedList.prototype.removeHead = function(){
+    // two cases: empty list and not empty list
+    if(!this.head) return null;
+    // list is not empty
+    var val = this.head.value;
+    this.head = this.head.next;
+    if(this.head){
+        this.head.previous = null;
+    }
+    else{
+        this.tail = null;
+    }
+    return val;
+}
+
+LinkedList.prototype.removeTail = function(){
+    if(!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.previous;
+    if(this.tail){
+        this.tail.next = null;
+    }
+    else{
+        this.head = null;
+    }
+    return val;
+}
+
+LinkedList.prototype.search = function(searchValue){
+    var currentNode = this.head;
+    while(currentNode){
+        if(currentNode.value == searchValue){
+            return currentNode.value;
+        }
+        currentNode = currentNode.next;
+    }
+}
+
+LinkedList.prototype.indexOf = function(value){
+    var arr = [];
+    var currentIndex = 0;
+    var currentNode = this.head;
+    while(currentNode){
+        if(currentNode.value === value){
+            arr.push(currentIndex);
+        }
+        currentNode = currentNode.next;
+        currentIndex++;
+    }
+    return arr;
+}
 // var ll = new LinkedList();
 // ll.addToHead(100);
 // ll.addToHead(200);
@@ -45,11 +96,47 @@ LinkedList.prototype.addToTail = function(value){
 
 // console.log(ll);
 
-var myLL = new LinkedList();
-myLL.addToTail(10);
-myLL.addToTail(20);
-myLL.addToTail(30);
+// var myLL = new LinkedList();
+// myLL.addToTail(10);
+// myLL.addToTail(20);
+// myLL.addToTail(30);
 
-myLL.addToHead(100);
+// myLL.addToHead(100);
 
-console.log(myLL);
+//var ll = new LinkedList();
+// ll.addToHead(1000);
+// ll.addToHead(2000);
+// ll.addToTail(3000);
+// ll.removeHead();
+// console.log(ll.removeHead())
+
+// ll.addToHead("one");
+// ll.addToHead("two");
+// ll.addToHead("three");
+
+// console.log(ll.removeTail());
+
+// var myLL = new LinkedList();
+
+// myLL.addToHead(123);
+// myLL.addToHead(70);
+// myLL.addToHead('hello');
+// myLL.addToTail(19);
+// myLL.addToTail('world');
+// myLL.addToTail(20);
+
+// console.log(myLL.search(70));
+// console.log(myLL.search("world"));
+// console.log(myLL.search(10));
+// console.log(myLL);
+
+var myLL = new LinkedList()
+myLL.addToTail(1);
+myLL.addToTail(5);
+myLL.addToTail(3);
+myLL.addToTail(5);
+myLL.addToTail(8);
+myLL.addToTail(7);
+myLL.addToTail(5);
+
+console.log(myLL.indexOf(5));
